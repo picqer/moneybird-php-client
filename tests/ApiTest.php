528ed58c2011340cf6219c0e4e4de1c432a17ca0
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2017-04-12 00:23:31 +0200

    Add initial approach to API-testing using mocks

---
+++ tests/ApiTest.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Class ApiTest
+ *
+ * Sets up a mocked Connection object. Each test case:
+ *  - indicates what the mocked Connection object should expect when interacting with the Client / Entities
+ *  - actually interact with the Client / Entities
+ */
+class ApiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Picqer\Financials\Moneybird\Moneybird
+     */
+    private $client;
+
+    /**
+     * @var \Picqer\Financials\Moneybird\Connection
+     */
+    private $connection;
+
+    /**
+     * Same as self::$connection, only differently typed to enable autocompletion in IDE
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockedConnection;
+
+    public function setUp()
+    {
+        $this->connection = $this->getMockBuilder(\Picqer\Financials\Moneybird\Connection::class)->getMock();
+        $this->connection->setTesting(true);
+        $this->mockedConnection = $this->connection;
+        $this->client = new \Picqer\Financials\Moneybird\Moneybird($this->connection);
+    }
+
+    public function testConnectionIsProperlyMocked()
+    {
+        $this->assertInstanceOf(\Picqer\Financials\Moneybird\Connection::class, $this->connection);
+    }
+
+    public function testTestModeIsProperlySet()
+    {
+        $this->mockedConnection->expects($this->once())
+            ->method('isTesting')
+            ->will($this->returnValue(true));
+
+        $this->assertTrue($this->connection->isTesting());
+    }
+
+    public function testSetAdministrationIdSet()
+    {
+        $administrationId = 123456789;
+        $this->mockedConnection->expects($this->once())
+            ->method('setAdministrationId')
+            ->with($administrationId);
+
+        $this->connection->setAdministrationId($administrationId);
+    }
+
+    public function testFindAllWebHooksTriggersHttpGet()
+    {
+        $attributes = [
+            'url' => 'https://www.domain.ext',
+        ];
+
+        $this->mockedConnection->expects($this->once())
+            ->method('get')
+            ->will($this->returnValue($attributes));
+
+        $webHook = $this->client->webhook($attributes);
+        $webHook->get();
+    }
+
+    public function testStoreWebHookTriggersHttpPost()
+    {
+        $attributes = [
+            'url' => 'https://www.domain.ext',
+        ];
+
+        $this->mockedConnection->expects($this->once())
+            ->method('post')
+            ->will($this->returnValue($attributes));
+
+        $webHook = $this->client->webhook($attributes);
+        $webHook->save();
+    }
+
+    public function testFinancialMutationLinkToBooking()
+    {
+        $financialMutationId = 1;
+        $bookingType = 'LedgerAccount';
+        $bookingId = 100;
+        $priceBase = 100.00;
+        $parameters = [
+            'booking_type' => $bookingType,
+            'booking_id' => $bookingId,
+            'price_base' => $priceBase,
+        ];
+        $httpResponseCode = 200;
+
+        $this->mockedConnection->expects($this->once())
+            ->method('patch')
+            ->with('financial_mutations/' . $financialMutationId . '/link_booking', json_encode($parameters))
+            ->will($this->returnValue($httpResponseCode));
+
+        $financialMutation = $this->client->financialMutation();
+        $financialMutation->id = $financialMutationId;
+        $this->assertEquals($httpResponseCode, $financialMutation->linkToBooking($bookingType, $bookingId, $priceBase));
+    }
+}
