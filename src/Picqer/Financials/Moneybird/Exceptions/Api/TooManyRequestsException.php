4f1468f5875e9af4a0b4f11c09d03547862903e2
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2017-10-03 23:12:45 +0200

    No need for static function

--- src/Picqer/Financials/Moneybird/Exceptions/Api/TooManyRequestsException.php
+++ src/Picqer/Financials/Moneybird/Exceptions/Api/TooManyRequestsException.php
@@ -7,23 +7,10 @@ use Picqer\Financials\Moneybird\Exceptions\ApiException;
 class TooManyRequestsException extends ApiException
 {
     /**
+     * @link https://developer.moneybird.com/#throttling
+     *
      * @var int
      */
     public $retryAfterNumberOfSeconds;
 
-    /**
-     * @link https://developer.moneybird.com/#throttling
-     *
-     * @param int $retryAfterNumberOfSeconds
-     *
-     * @return TooManyRequestsException
-     */
-    public static function retryAfter($retryAfterNumberOfSeconds)
-    {
-        $exceptionMessage = sprintf('Too many requests received, API throttling has been enabled, please try again in %d seconds', $retryAfterNumberOfSeconds);
-        $exception = new self($exceptionMessage);
-        $exception->retryAfterNumberOfSeconds = $retryAfterNumberOfSeconds;
-
-        return $exception;
-    }
 }
