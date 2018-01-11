f046023672d2e3daa1b3a29b954280c023185503
Stephan Groen <stephan@picqer.com>
2018-01-11 10:34:43 +0100

    Fixes tests for PHP56

--- src/Picqer/Financials/Moneybird/Entities/RecurringSalesInvoiceCustomField.php
+++ src/Picqer/Financials/Moneybird/Entities/RecurringSalesInvoiceCustomField.php
@@ -1,11 +1,9 @@
 <?php namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Entities\Generic\CustomField;
-
 /**
  * Class RecurringSalesInvoiceCustomField
  * @package Picqer\Financials\Moneybird\Entities
  */
-class RecurringSalesInvoiceCustomField extends CustomField
+class RecurringSalesInvoiceCustomField extends Generic\CustomField
 {
 }
--- src/Picqer/Financials/Moneybird/Entities/SalesInvoiceCustomField.php
+++ src/Picqer/Financials/Moneybird/Entities/SalesInvoiceCustomField.php
@@ -1,11 +1,9 @@
 <?php namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Entities\Generic\CustomField;
-
 /**
  * Class SalesInvoiceCustomField
  * @package Picqer\Financials\Moneybird\Entities
  */
-class SalesInvoiceCustomField extends CustomField
+class SalesInvoiceCustomField extends Generic\CustomField
 {
 }
