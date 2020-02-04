addc1cc174c5d66fa61ec25d0ce21bbaf6f8e24d
Manuel de Ruiter <manuel@inquiry.center>
2020-02-04 07:18:26 +0100

    With this commit we add support for adding attachments to a receipt

--- src/Picqer/Financials/Moneybird/Entities/Receipt.php
+++ src/Picqer/Financials/Moneybird/Entities/Receipt.php
@@ -2,19 +2,20 @@
 
 namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Model;
+use Picqer\Financials\Moneybird\Actions\Attachment;
 use Picqer\Financials\Moneybird\Actions\FindAll;
 use Picqer\Financials\Moneybird\Actions\FindOne;
 use Picqer\Financials\Moneybird\Actions\Storable;
 use Picqer\Financials\Moneybird\Actions\Removable;
 use Picqer\Financials\Moneybird\Exceptions\ApiException;
+use Picqer\Financials\Moneybird\Model;
 
 /**
  * Class Receipt.
  */
 class Receipt extends Model
 {
-    use FindAll, FindOne, Storable, Removable;
+    use FindAll, FindOne, Storable, Removable, Attachment;
 
     /**
      * @var array
