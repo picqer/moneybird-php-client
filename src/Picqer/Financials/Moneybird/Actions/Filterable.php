13a1fffcf54963e065435473e38f72efef151be3
Csaba Farago <oppika0212@gmail.com>
2020-06-22 09:00:22 +0100

    Add pager to invoice export

--- src/Picqer/Financials/Moneybird/Actions/Filterable.php
+++ src/Picqer/Financials/Moneybird/Actions/Filterable.php
@@ -11,17 +11,28 @@ trait Filterable
 
     /**
      * @param array $filters
+     * @param array $pager_params
      * @return mixed
      * @throws \Picqer\Financials\Moneybird\Exceptions\ApiException
      */
-    public function filter(array $filters)
+    public function filter(array $filters, $pager_params = false)
     {
         $filterList = [];
         foreach ($filters as $key => $value) {
-            $filterList[] = $key . ':' . $value;
+            $filterList[] = $key .':' . $value;
         }
 
-        $result = $this->connection()->get($this->getEndpoint(), ['filter' => implode(',', $filterList)]);
+        $params['filter'] = implode(',', $filterList);
+        if(isset($pager_params)){
+            if(isset($pager_params['page'])){
+                $params['page'] = $pager_params['page'];
+            }
+            if(isset($pager_params['per_page'])){
+                $params['per_page'] = $pager_params['per_page'];
+            }
+        }
+
+        $result = $this->connection()->get($this->getEndpoint(), $params);
 
         return $this->collectionFromResult($result);
     }
