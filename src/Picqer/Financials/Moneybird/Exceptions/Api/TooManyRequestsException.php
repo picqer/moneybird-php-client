5efc60cfc329ddd5bb3c37331162f0fbbbc4e534
gldrenthe89 <56250338+gldrenthe89@users.noreply.github.com>
2020-06-24 23:59:33 +0200

    Update TooManyRequestsException
    added new variables to reflect changes on Moneybird API

--- src/Picqer/Financials/Moneybird/Exceptions/Api/TooManyRequestsException.php
+++ src/Picqer/Financials/Moneybird/Exceptions/Api/TooManyRequestsException.php
@@ -7,9 +7,26 @@ use Picqer\Financials\Moneybird\Exceptions\ApiException;
 class TooManyRequestsException extends ApiException
 {
     /**
+     * Moneybird Ratelimit header: RateLimit-Remaining
      * @link https://developer.moneybird.com/#throttling
      *
      * @var int
      */
     public $retryAfterNumberOfSeconds;
+
+    /**
+     * Moneybird Ratelimit header: RateLimit-Limit
+     * @link https://developer.moneybird.com/#throttling
+     *
+     * @var int
+     */
+    public $currentRateLimit;
+
+    /**
+     * Moneybird Ratelimit header: RateLimit-Reset
+     * @link https://developer.moneybird.com/#throttling
+     *
+     * @var int
+     */
+    public $rateLimitResetsAfterTimestamp;
 }
