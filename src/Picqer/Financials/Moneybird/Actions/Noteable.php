fdffcd40651ab76e1e2d7374007bce8ffb402046
Robin Dirksen <robin@famdirksen.nl>
2020-09-08 16:53:42 +0200

    Create Noteable.php

---
+++ src/Picqer/Financials/Moneybird/Actions/Noteable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Picqer\Financials\Moneybird\Actions;
+
+use Picqer\Financials\Moneybird\Entities\Contact;
+use Picqer\Financials\Moneybird\Entities\Note;
+use Picqer\Financials\Moneybird\Exceptions\ApiException;
+
+/**
+ * Class Noteable.
+ */
+trait Noteable
+{
+    use BaseTrait;
+
+    /**
+     * Add a note to the current object.
+     *
+     * @param Note $note
+     * @return $this
+     * @throws ApiException
+     */
+    public function addNote(Note $note)
+    {
+        $this->connection()->post($this->getEndpoint() . '/' . urlencode($this->id) . '/notes',
+            $note->jsonWithNamespace()
+        );
+
+        return $this;
+    }
+}
