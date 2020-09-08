e9211d8c5357034b2bc013bdedc1974376f0b6c7
Robin Dirksen <robindirksen1@users.noreply.github.com>
2020-09-08 15:14:14 +0000

    Apply fixes from StyleCI

--- src/Picqer/Financials/Moneybird/Entities/GeneralJournalDocument.php
+++ src/Picqer/Financials/Moneybird/Entities/GeneralJournalDocument.php
@@ -2,9 +2,9 @@
 
 namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Actions\Noteable;
 use Picqer\Financials\Moneybird\Actions\FindAll;
 use Picqer\Financials\Moneybird\Actions\FindOne;
+use Picqer\Financials\Moneybird\Actions\Noteable;
 use Picqer\Financials\Moneybird\Actions\Removable;
 use Picqer\Financials\Moneybird\Actions\Storable;
 use Picqer\Financials\Moneybird\Model;
--- src/Picqer/Financials/Moneybird/Entities/PurchaseInvoice.php
+++ src/Picqer/Financials/Moneybird/Entities/PurchaseInvoice.php
@@ -2,11 +2,11 @@
 
 namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Actions\Noteable;
 use Picqer\Financials\Moneybird\Actions\Attachment;
 use Picqer\Financials\Moneybird\Actions\Filterable;
 use Picqer\Financials\Moneybird\Actions\FindAll;
 use Picqer\Financials\Moneybird\Actions\FindOne;
+use Picqer\Financials\Moneybird\Actions\Noteable;
 use Picqer\Financials\Moneybird\Actions\Removable;
 use Picqer\Financials\Moneybird\Actions\Storable;
 use Picqer\Financials\Moneybird\Actions\Synchronizable;
