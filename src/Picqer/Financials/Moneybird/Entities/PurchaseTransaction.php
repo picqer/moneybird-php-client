247528b9de5d4ec9ac57a713ce4cc36f575b79c5
Geert Lucas <56250338+gldrenthe89@users.noreply.github.com>
2024-08-07 22:28:19 +0200

    CI fix

--- src/Picqer/Financials/Moneybird/Entities/PurchaseTransaction.php
+++ src/Picqer/Financials/Moneybird/Entities/PurchaseTransaction.php
@@ -2,55 +2,106 @@
 
 namespace Picqer\Financials\Moneybird\Entities;
 
-use Picqer\Financials\Moneybird\Actions\Attachment;
-use Picqer\Financials\Moneybird\Actions\Filterable;
 use Picqer\Financials\Moneybird\Actions\FindAll;
 use Picqer\Financials\Moneybird\Actions\FindOne;
-use Picqer\Financials\Moneybird\Actions\Noteable;
 use Picqer\Financials\Moneybird\Actions\Removable;
-use Picqer\Financials\Moneybird\Actions\Storable;
-use Picqer\Financials\Moneybird\Actions\Synchronizable;
-use Picqer\Financials\Moneybird\Exceptions\ApiException;
 use Picqer\Financials\Moneybird\Model;
 
 /**
  * Class PurchaseInvoice.
  */
-class PurchaseTransaction extends Model
+class PurchaseInvoice extends Model
 {
-    use FindAll, FindOne, Removable;
+    use FindAll, FindOne, Storable, Removable, Filterable, Synchronizable, Attachment, Noteable;
 
     /**
      * @var array
      */
     protected $fillable = [
         'id',
-        'financial_account_id',
-        'payment_instrument_id',
-        'state',
-        'sepa_iban',
-        'sepa_iban_account_name',
-        'sepa_bic',
-        'source_sepa_iban',
-        'source_sepa_iban_account_name',
+        'contact_id',
+        'reference',
         'date',
-        'description',
-        'end_to_end_id',
-        'amount',
+        'due_date',
+        'entry_number',
+        'state',
+        'currency',
+        'exchange_rate',
+        'revenue_invoice',
+        'prices_are_incl_tax',
+        'origin',
+        'paid_at',
+        'tax_number',
+        'total_price_excl_tax',
+        'total_price_excl_tax_base',
+        'total_price_incl_tax',
+        'total_price_incl_tax_base',
         'created_at',
         'updated_at',
-        'payable_type',
-        'payable_id',
-        'payment_method',
+        'details',
+        'payments',
+        'notes',
+        'attachments',
+        'version',
     ];
 
     /**
      * @var string
      */
-    protected $endpoint = 'purchase_transactions';
+    protected $endpoint = 'documents/purchase_invoices';
 
     /**
      * @var string
      */
-    protected $namespace = 'purchase_transaction';
+    protected $namespace = 'purchase_invoice';
+
+    /**
+     * @var array
+     */
+    protected $singleNestedEntities = [
+        'contact' => Contact::class,
+    ];
+
+    /**
+     * @var array
+     */
+    protected $multipleNestedEntities = [
+        'attachments' => [
+            'entity' => PurchaseInvoiceAttachment::class,
+            'type' => self::NESTING_TYPE_ARRAY_OF_OBJECTS,
+        ],
+        'details' => [
+            'entity' => PurchaseInvoiceDetail::class,
+            'type' => self::NESTING_TYPE_ARRAY_OF_OBJECTS,
+        ],
+        'payments' => [
+            'entity' => PurchaseInvoicePayment::class,
+            'type' => self::NESTING_TYPE_ARRAY_OF_OBJECTS,
+        ],
+    ];
+
+    /**
+     * Register a payment for the current purchase invoice.
+     *
+     * @param  PurchaseInvoicePayment  $purchaseInvoicePayment  (payment_date and price are required)
+     * @return $this
+     *
+     * @throws ApiException
+     */
+    public function registerPayment(PurchaseInvoicePayment $purchaseInvoicePayment)
+    {
+        if (! isset($purchaseInvoicePayment->payment_date)) {
+            throw new ApiException('Required [payment_date] is missing');
+        }
+
+        if (! isset($purchaseInvoicePayment->price)) {
+            throw new ApiException('Required [price] is missing');
+        }
+
+        $this->connection()->patch($this->endpoint . '/' . $this->id . '/register_payment',
+            $purchaseInvoicePayment->jsonWithNamespace()
+        );
+
+        return $this;
+    }
 }
